07aac9b2cc093a92644fd2e897b8954783147df0
SarahYuHanCheng <bigbigtreeful@gmail.com>
2017-09-05 22:41:12 +0800

    edit first multi_thread

--- hello_multi_thread.py
+++ hello_multi_thread.py
@@ -7,7 +7,7 @@ def client_thread():
     print('This is client_thread')
 
 
-# # task:  server
+''' main thread task: server '''
 def server():
 
         global threads 
@@ -26,7 +26,6 @@ def server():
         # 產生第二支子執行緒
         childThread_client_thread = threading.Thread(target=client_thread, args=())
         childThread_client_thread.start()
-        childThread_client_thread.join()
         threads.append(childThread_client_thread)
         j = 0
         for t in threads:
@@ -36,17 +35,25 @@ def server():
         # 產生第三支子執行緒
         childThread_client_thread = threading.Thread(target=client_thread, args=())
         childThread_client_thread.start()
-        # enumerate() return a list, include所有alive的thread objects 以及 main thread.
-        # (alive thread includes daemon and dummy threads)
-        alives = threading.enumerate()
-        print(alives)
         threads.append(childThread_client_thread)
         j = 0
         for t in threads:
         	print(threads[j])
         	j +=1
 
-# 程式從這裡開始跑
-# 產生一支主執行緒main thread
+
+
+
+'''程式從這裡開始跑，產生一支主執行緒main thread'''
+# target is the callable object to be invoked by the run() method.
 mainThread_server = threading.Thread(target=server, args=())
+# calling the thread’s start() method invokes the run() method in a separate thread of control.
 mainThread_server.start()
+
+# enumerate() return a list, include所有alive的thread objects 以及 main thread.
+# (alive thread includes daemon and dummy threads)
+alives = threading.enumerate()
+print(alives)
+
+mainThread_server.join()
+
