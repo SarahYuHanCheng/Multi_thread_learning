c232eb38a2eba17ddf0e0bddf24394f243899a6b
SarahCheng <bigbigtreeful@gmail.com>
2017-10-10 10:45:28 -0500

    Can't run systemcall

--- multithread_c.c
+++ multithread_c.c
@@ -1,78 +1,51 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 
+void *runner(); /* the thread */
 #define M 3
 #define K 2
 #define N 3
 #define NUM_THREADS M * N
 
-/* Global variables for threads to share */
-int A[M][K] = {{1, 4}, {2, 5}, {3, 6}};
-int B[K][N] = {{8, 7, 6}, {5, 4, 3}};
-int C[M][N];
+// serverIP
+// socket_set
 
-/* Structure for passing data to threads */
-struct v
-{
-	int i; /* row */
-	int j; /* column */
-};
-
-void *runner(void *ptr); /* the thread */
-
-int main(int argc, char **argv)
-{
-	int i, j;
-	int thread_counter = 0;
-	
+int main(){
+// system("ls");
+	int intr=1;
+	// system("gcc server/testc.c -o server/testc");
+	// system("./server/testc");
+	// system("gcc server/2test.c -o server/2test");
+	// system("./server/2test");
 	pthread_t workers[NUM_THREADS];
-	
-	/* We have to create M * N worker threads */
-	for (i = 0; i < M; i++)
-	{
-		for (j = 0; j < N; j++) 
+	int thread_num;
+	thread_num=0;
+	int tt=0;
+	while(tt<2){
+		if (intr==1)
 		{
-			struct v *data = (struct v *) malloc(sizeof(struct v));
-			data->i = i;
-			data->j = j;
-			/* Now we will create the thread passing it data as a paramater*/
-			pthread_create(&workers[thread_counter], NULL, runner, data);
-			// pthread_join(workers[thread_counter], NULL);
-			thread_counter++;
+			if(thread_num==0){
+				thread_num=1;
+			}else{
+				thread_num=0;
+			}
+			pthread_create(&workers[tt], NULL, runner,NULL);
+			printf("%d\n",tt);
 		}
-
+		tt++;
 	}
-	
-	/* Waiting for threads to complete */
-	for (i = 0; i < NUM_THREADS; i++)
-	{
-	    pthread_join(workers[i], NULL);
-	}
-	
-	for(i = 0; i < M; i++)
-	{ 
-		for(j = 0; j < N; j++)
-		{ 
-			printf("%d\t", C[i][j]);
-		}
-		printf("\n");
-	}
-	return 0;
 }
-
-void *runner(void *ptr)
+void *runner()
 {	
 	/* Casting paramater to struct v pointer */
-	struct v *data = ptr;
-	int i, sum = 0;
-	
-	for(i = 0; i < K; i++)
-	{	
-		sum += A[data->i][i] * B[i][data->j];
-	}
-	
-	C[data->i][data->j] = sum;
-	printf("%d,running:%d\n", sum,pthread_self());	
-	pthread_exit(0);
+	// if (num==0)
+	// {
+		printf("0000\n");
+		system("gcc server/testc.c -o server/testc");
+		system("./server/testc");
+		
+	// }else{
+	// 	system("gcc server/2test.c -o server/2test");
+	// 	system("./server/2test");
+	// }
 }
